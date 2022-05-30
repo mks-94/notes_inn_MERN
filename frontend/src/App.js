8eaa6fd244573517e93e0a147918d704d9e14799
Murali krishna <muralikrishna9424@gmail.com>
2022-05-30 22:04:04 +0530

    Implemented user authentication.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
 import MyNotes from "./screens/MyNotes/MyNotes";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" exact element={<LandingPage />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
           <Route path="/mynotes" element={<MyNotes />} />
         </Routes>
       </main>
